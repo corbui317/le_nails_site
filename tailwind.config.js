49035437baa20c663674903e50fd1546c5982849
Corey Bui <corbui317@gmail.com>
2025-06-20 20:49:51 -0500

    loaded fonts, new script for configured tailwind, updated headers and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-module.exports = { content: ['./src/**/*.{js,jsx}'], theme: { extend: {} }, plugins: [] };
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        script: ['"Dancing Script"', 'cursive'],
+        body: ['"Open Sans"', 'sans-serif'],
+      },
+      colors: {
+        primary: '#d1a3a4',
+        accent: '#f1e4e6',
+      },
+    },
+  },
+  plugins: [],
+};
