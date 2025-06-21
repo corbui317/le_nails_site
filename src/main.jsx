057ba68ef6cca64296e5d0dbda50f3f41e2796c7
Corey Bui <corbui317@gmail.com>
2025-06-20 21:50:07 -0500

    changes

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LandingPage from "./pages/index.jsx";
+import "./assets/styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>
+);
